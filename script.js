0f0bac19b7431a157260947a680fef7ce7d2ee29
SerGo3580 <77640910+SerGo3580@users.noreply.github.com>
2021-02-15 00:45:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 var first;
-function bye (name){
-    console.log("Bye "+name);
-}
-function hello(name){
-	console.log ("Hello " + name)
-}
+var byeSpeaker = {
+	speak:function (name){
+		console.log("Bye "+name);
+	}
+};
+var helloSpeaker = {
+	speak:function (name){
+		console.log("Hello "+name);
+	}
+};
+
 for (var i in  name){
     first = name[i].charAt(0);
     if (first==='j' || first === 'J'){
-    	bye(name[i]);
+    	byeSpeaker.speak(name[i]);
     }
     else {
-       hello(name[i]);
+       helloSpeaker.speak(name[i]);
     }
 } 
